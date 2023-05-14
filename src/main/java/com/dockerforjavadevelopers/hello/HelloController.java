d57e3e21910e337691f03b69465af39c9f3b7c12
Thirumurthy <91thiru@gmail.com>
2023-05-14 09:59:42 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -2,14 +2,20 @@ package com.dockerforjavadevelopers.hello;
 
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
     
     @RequestMapping("/")
-    public String index() {
-        return "Hello World\n";
+    public Map<String, String> index() {
+    	HashMap<String, String> map = new HashMap<>();
+        map.put("result", "Hello World from java");
+        return map;
     }
     
 }
